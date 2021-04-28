f20de8453e6b94ded10e1af168f750594ccdf27c
WILLIAM LEE GUAN SHENG <20040896@myrp.edu.sg>
2021-04-28 15:20:24 +0800

    Initial commit

---
+++ app/src/main/java/sg/rp/edu/c346/id20040896/registrationapp/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.rp.edu.c346.id20040896.registrationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
